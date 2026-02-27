2b8b8c3eec286078e955de00853dff3256f43ffa
Khaliel Myrie <khalamyrie@gmail.com>
2026-02-27 10:23:13 -0500

    make default setup for arrows and slide indicators

---
+++ slide-indicator.js
@@ -0,0 +1,41 @@
+import { LitElement, html, css } from "lit";
+import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+
+export class SlideIndicator extends DDDSuper(I18NMixin(LitElement)) {
+    static get tag() {
+        return "slide-indicator";
+    }
+
+    constructor() {
+        super();
+    }
+
+    static get properties() {
+        return {
+            ...super.properties,
+        };
+    }
+
+    static get styles() {
+        return [super.styles,
+        css`
+        /* nothing in here yet
+        :host {
+        
+        }*/
+        `];
+    }
+
+    render() {
+        return html`
+        `;
+    }
+
+    static get haxProperties() {
+        return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+            .href;
+    }
+}
+
+globalThis.customElements.define(SlideIndicator.tag, SlideIndicator);
