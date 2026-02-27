f06125e72dce70ed5a168291c34564dc3e9409fc
Khaliel Myrie <khalamyrie@gmail.com>
2026-02-26 19:28:40 -0500

    lowkey broken but we't figure it out

--- play-list-project.js
+++ play-list-project.js
@@ -20,23 +20,12 @@ export class PlayListProject extends DDDSuper(I18NMixin(LitElement)) {
   
   constructor() {
     super();
-    this.topHeading = "Top Line Heading";
-    this.subheading = "Slide Subheading";
-    this.activeSlide = 0;
-    this.details = "Information for slide.";
-    this.title = "Screenreader accessibility";
   }
 
   // Lit reactive properties
   static get properties() {
     return {
       ...super.properties,
-      topHeading: { type : String },
-      subheading: { type : String },
-      activeSlide: { type : Number },
-      items: { type: Array },
-      details: { type: String },
-      title: { type: String }
     };
   }
 
@@ -50,50 +39,18 @@ export class PlayListProject extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      
-      .top-heading{
-        font-size: var(--ddd-play-list-project-title-font-size, var(--ddd-font-size-s));
-      }
-      .subheading {
-        font-size: var(--ddd-play-list-project-subheading-font-size, var(--ddd-font-size-l));
-      }
-      hr {
-        width: 80px;
-        min-height: 1px;
-        padding-top: 4px;
-        padding-bottom: 4px;
-        margin-left: 0;
-        margin-right: auto;
-        color: var(--ddd-theme-default-original87Pink);
-        margin-top: 4px;
-      }
-      .details {
-        min-height: 200px;
-        max-height: 200px;
-        max-width: 450px;
-        text-align: left;
-        overflow-y: auto;
-      }
     `];
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="play-list-slide" title="${this.title}">
-  <div class="wrapper">
-    <span class="top-heading">${this.topHeading}</span>
-    <br>
-    <span class="subheading">${this.subheading}</span>
-    <hr>
-    <p class="details">${this.details}</p>
-    <slot></slot>
-  </div>
-</div>`;
+      <play-list-slide
+        topHeading = "ETI 301"
+        subheading = "College of IST"
+        details = "blah blah blah blah blah blah">
+      </play-list-slide>
+    `;
   }
 
   /**
